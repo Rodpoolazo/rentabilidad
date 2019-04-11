83928898996b61530ed5f3ed4662dfb5d841e169
Rodpoolazo <rodpoolazo@gmail.com>
2019-04-11 19:51:03 -0400

    add impuestos y utilidad bruta

--- emprendedor2.rb
+++ emprendedor2.rb
@@ -4,6 +4,7 @@ precio_venta = ARGV[0].to_i
 usuario_normal = ARGV[1].to_i
 usuarios_premium = ARGV[2].to_i
 usuarios_gratuito = ARGV[3].to_i
+impuestos = 0.35
 gastos = ARGV[4].to_i
 
 ingresonormal = precio_venta*usuario_normal
@@ -15,3 +16,11 @@ ingresogratuito = 0*precio_venta*usuarios_gratuito
 utilidad =(ingresonormal+ingresopremium+ingresogratuito)-gastos
 
 puts utilidad
+
+if utilidad > 0
+    utilidad_bruta = utilidad-(utilidad*impuestos)
+    else
+    utilidad_bruta = 0
+end
+
+puts utilidad_bruta
